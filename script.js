dc53d7b6799c9cb13f31366227a6f200a2b96d84
Oli Mebberson <162764375+olii-dev@users.noreply.github.com>
2024-07-04 10:20:32 +0930

    Added thirteen items, centered all of the items
    I added thirteen items, Breadboard + jumper wires, Multimeter, Arcade Ticket Counter, YubiKey, Soldering iron + solder, Raspberry Pi, Pinecil, GitHub Keycaps x8, Wacom Intuos S, Octocat, Keychron K6 Pro, Flipper Zero, Logitech MX Mechanical.
    
    I also centered all of the items to the middle of the screen for a cleaner look.

--- script.js
+++ script.js
@@ -26,6 +26,45 @@ document.addEventListener('DOMContentLoaded', function() {
         if (itemName.includes('logic analyser')) {
             maxQuantity = 5;
         }
+        if (itemName.includes('breadboard + jumper wires')) {
+            maxQuantity = 10;
+        }
+        if (itemName.includes('multimeter')) {
+            maxQuantity = 5;
+        }
+        if (itemName.includes('arcade ticket counter')) {
+            maxQuantity = 1;
+        }
+        if (itemName.includes('yubikey')) {
+            maxQuantity = 5;
+        }
+        if (itemName.includes('soldering iron + solder')) {
+            maxQuantity = 5;
+        }
+        if (itemName.includes('raspberry pi')) {
+            maxQuantity = 1;
+        }
+        if (itemName.includes('pinecil')) {
+            maxQuantity = 1;
+        }
+        if (itemName.includes('github keycaps x8')) {
+            maxQuantity = 1;
+        }   
+        if (itemName.includes('wacom intuos s')) {
+        maxQuantity = 1;
+        }   
+        if (itemName.includes('octocat')) {
+        maxQuantity = 1;
+        }
+        if (itemName.includes('keychron k6 pro')) {
+            maxQuantity = 1;
+        }
+        if (itemName.includes('flipper zero')) {
+            maxQuantity = 1;
+        }
+        if (itemName.includes('logitech mx mechanical')) {
+            maxQuantity = 1;
+        }   
 
         function updateQuantity() {
             quantityBtn.textContent = quantity;
